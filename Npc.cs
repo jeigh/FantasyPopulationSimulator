29d8e1adf8931b1eaac7105da7f20dee6563b5e7
Jason <j-folkens@hotmail.com>
2025-05-14 21:26:29 -0400

    moved most methods over to npcbehavior to split state form behavior

--- Npc.cs
+++ Npc.cs
@@ -1,149 +1,72 @@
 ﻿using FantasyPopulationSimulator.Console.Interfaces;
-using static FantasyPopulationSimulator.Console.Constants.GlobalConstants;
 using FantasyPopulationSimulator.Console.Constants;
-using FantasyPopulationSimulator.Console.Traits;
 
 namespace FantasyPopulationSimulator.Console
 {
+
     public class Npc : ITickable
     {
-        private readonly PopulationTracker _pop;
-        private readonly ConsoleUI _ui;
-        private readonly RandomNumberGenerator _rand;
+        private readonly NpcBehavior _behavior;
+        
+        private readonly Npc? _mother;
+        private readonly Npc? _father;
 
         public Npc(PopulationTracker pop, Npc mother, Npc? father, ConsoleUI ui, RandomNumberGenerator rand)
         {
-            _pop = pop;
-            _ui = ui;
-            _rand = rand;
+            _behavior = new NpcBehavior(pop, ui, rand);
 
             _mother = mother;
             _father = father;
 
-            _race = _mother._race.CreatesChildrenOfRace();
+            Race = _mother.Race.CreatesChildrenOfRace();
             
             Culture = _mother.Culture;                  // todo: matrilineal or patrilineal culture?
-            _currentZone = _mother.GetCurrentZone();    // todo: matrilineal or patrilineal zone?
-
+            CurrentZone = _mother.CurrentZone;    // todo: matrilineal or patrilineal zone?
         }
 
         public Npc(PopulationTracker pop, IRace race, ICulture culture, IZone currentZone, ConsoleUI ui, RandomNumberGenerator rand)
         {
-            _pop = pop;
-            _race = race;
-            _rand = rand;
+            _behavior = new NpcBehavior(pop, ui, rand);
+
+            Race = race;
             Culture = culture;
-            _currentZone = currentZone;
-            _ui = ui;
+            CurrentZone = currentZone;
         }
 
-        private readonly IRace _race;
-
-        private Npc? _mother { get; set; } = null;
-        private Npc? _father { get; set; } = null;
-
         public ICulture Culture { get; set; } = null;
+        public IRace Race { get; private set; }
 
         public string FirstName { get; set; } = string.Empty;
         public string? LastName { get; set; } // todo: make last name not nullable
         public int AgeInDays { get; set; } = 0;
         public Sex Sex { get; set; } = Sex.None;
-        public int BirthDay()  => (int)(BirthDate % DaysInYear);
-        private IZone _currentZone;
-        public IZone GetCurrentZone() => _currentZone;
+        
+        public IZone CurrentZone { get; private set; }
 
         public long BirthDate { get; set; } = 0;
 
-        private long _lastPregnancyEnded { get; set; } = 0;
-        private long _lastImpregnatedOn { get; set; } = 0;
+        public long LastPregnancyEnded { get; set; } = 0;
+        public long LastImpregnatedOn { get; set; } = 0;
 
-        public bool IsPregnant() => _lastImpregnatedOn > 0;
-        public void Impregnate(long currentDate) => _lastImpregnatedOn = currentDate;  //todo: expand this feature so as to allow twins?
-        public bool IsFertile()
-        {
-            return
-                AgeInDays >= _race.StartOfFertility &&
-                AgeInDays < _race.EndOfFertility;
-        }
-
-        public void ResetPregnancy(long today)
-        {
-            _lastPregnancyEnded = today; //todo: make this a long
-            _lastImpregnatedOn = 0;
-        }
-
-
-        public void BlockUntilTickCompletes(long today)
-        {
-            if (TimeToDie(today))
-            {
-                Die();
-                return;
-            }
-
-            if (BirthDay() == today % DaysInYear) _ui.NpcBirthday();
-            if (CanGiveBirthToday(today)) GiveBirth(today);
-            if (CanGetPregnant(today)) Impregnate(today);
-            if (IsAdult(today))
-            {
-                if (ProbabilityOfGettingANewTraitAtDay(AgeInDays) >= _rand.GeneratePercentage())
-                    GiveTrait(new WandererTrait());
-            }
-
-            AgeInDays++;
-        }
+        public bool IsPregnant() => LastImpregnatedOn > 0;
 
         private List<ITrait> Traits { get; set; } = new List<ITrait>();
 
-        private void GiveTrait(ITrait trait) => Traits.Add(trait);
+        public void GiveTrait(ITrait trait) => Traits.Add(trait);
 
         public bool HasTrait(ITrait trait) => Traits.Contains(trait);
-
-        // todo: use calculus to return what the equasion should be when the cdf (or more accurately, integral of f(g)) goes from
-        // f(g)=0 when g= (350*18) to f(g)=0.10 when g = (350*80) instead of just approximating a hard coded value
-        //
-        // using a hard coded value assumes an equal chance that someone will gain a trait throughout their life
-        // but I believe people are more likely to get their traits sooner in life
-        public double ProbabilityOfGettingANewTraitAtDay(int ageInDays) =>
-             0.00001f;   //small probability of occurring each day
-         
-
-        private void Die()
-        {
-            _ui.NpcDeath();
-            _pop.Remove(this);
-        }
-
-        private bool TimeToDie(long today) => AgeInDays >= _race.DiesAtAge;
-
-        private bool CanGiveBirthToday(long today) => 
-            IsPregnant() && today >= _lastImpregnatedOn + _race.PregnancyDurationInDays;
-
-        private void GiveBirth(long day)
-        {
-            ResetPregnancy(day);
-            
-            _pop.GenerateNewNpc(this, null, day); //todo: determine who the father is?
-        }
-
-        private bool CanGetPregnant(long today)   // todo: verify that a mate is nearby?
-        {
-            return (Sex == Sex.Female &&
-                IsFertile() && 
-                !IsPregnant() &&
-                _lastPregnancyEnded + _race.TimeBetweenPregnancies <= today);
-        }
-
-        public IRace GetRaceOfChildren() => 
-            _race.CreatesChildrenOfRace();  // todo: when two parents arent of the same race?
-
+        
         public long GetNpcCount() => 1;
-
-        public string GetAssignedZoneName() => string.Empty; // violation of liskov substitution principle
-
-        public bool IsAdult(long today) =>
-            AgeInDays >= _race.AdulthoodBeginsAt;
-
+        
+        public string GetAssignedZoneName() => 
+            string.Empty; // violation of liskov substitution principle
+        
+        public void BlockUntilTickCompletes(long day) => 
+            _behavior.BlockUntilTickCompletes(this, day);
     }
 
 }
+
+
+
+
---
+++ NpcBehavior.cs
@@ -0,0 +1,100 @@
+﻿using static FantasyPopulationSimulator.Console.Constants.GlobalConstants;
+using FantasyPopulationSimulator.Console.Constants;
+using FantasyPopulationSimulator.Console.Traits;
+
+namespace FantasyPopulationSimulator.Console
+{
+    public class NpcBehavior
+    {
+        private readonly PopulationTracker _pop;
+        private readonly ConsoleUI _ui;
+        private readonly RandomNumberGenerator _rand;
+
+        public NpcBehavior(PopulationTracker pop, ConsoleUI ui, RandomNumberGenerator rand)
+        {
+            _pop = pop;
+            _ui = ui;
+            _rand = rand;
+        }
+
+        public int BirthDay(Npc npc) => (int)(npc.BirthDate % DaysInYear);
+
+        public void BlockUntilTickCompletes(Npc npc, long today)
+        {
+            if (TimeToDie(npc, today))
+            {
+                Die(npc);
+                return;
+            }
+
+            if (BirthDay(npc) == today % DaysInYear) _ui.NpcBirthday();
+            if (CanGiveBirthToday(npc, today)) GiveBirth(npc, today);
+            if (CanGetPregnant(npc, today)) npc.LastImpregnatedOn = today;
+            if (IsAdult(npc, today))
+            {
+                if (ProbabilityOfGettingANewTraitAtDay(npc.AgeInDays) >= _rand.GeneratePercentage())
+                    npc.GiveTrait(new WandererTrait());
+            }
+
+            npc.AgeInDays++;
+        }
+
+        private bool TimeToDie(Npc npc, long today) => 
+            npc.AgeInDays >= npc.Race.DiesAtAge;
+
+        private bool CanGiveBirthToday(Npc npc, long today) =>
+            npc.IsPregnant() && today >= npc.LastImpregnatedOn + npc.Race.PregnancyDurationInDays;
+
+        private void Die(Npc npc)
+        {
+            _ui.NpcDeath();
+            _pop.Remove(npc);
+        }
+
+        public bool IsFertile(Npc npc)
+        {
+            return
+                npc.AgeInDays >= npc.Race.StartOfFertility &&
+                npc.AgeInDays < npc.Race.EndOfFertility;
+        }
+
+        // todo: use calculus to return what the equasion should be when the cdf (or more accurately, integral of f(g)) goes from
+        // f(g)=0 when g= (350*18) to f(g)=0.10 when g = (350*80) instead of just approximating a hard coded value
+        //
+        // using a hard coded value assumes an equal chance that someone will gain a trait throughout their life
+        // but I believe people are more likely to get their traits sooner in life
+        public double ProbabilityOfGettingANewTraitAtDay(int ageInDays) =>
+             0.00001f;   //small probability of occurring each day
+
+        private void GiveBirth(Npc npc, long day)
+        {
+            ResetPregnancy(npc, day);
+            _pop.GenerateNewNpc(npc, null, day); //todo: determine who the father is?
+        }
+
+        public void ResetPregnancy(Npc npc, long today)
+        {
+            npc.LastPregnancyEnded = today;
+            npc.LastImpregnatedOn = 0;
+        }
+
+        private bool CanGetPregnant(Npc npc, long today)   // todo: verify that a mate is nearby?
+        {
+            return (
+                npc.Sex == Sex.Female &&
+                IsFertile(npc) &&
+                !npc.IsPregnant() &&
+                npc.LastPregnancyEnded + npc.Race.TimeBetweenPregnancies <= today);
+        }
+
+        public bool IsAdult(Npc npc, long today) =>
+            npc.AgeInDays >= npc.Race.AdulthoodBeginsAt;
+
+
+    }
+
+}
+
+
+
+
